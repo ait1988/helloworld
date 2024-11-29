e164ac3c74abaae6b1740051d713364c7eea22e5
ait1988 <ait1988@gmail.com>
2024-11-29 13:25:46 +0700

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 import os
 
+
 def insecure_function():
     password = "hardcode password"
-    os.system(f'echo {password}')
+    os.system(f"echo {password}")
 
 
 def secure_function():
     password = os.getenv("PASSWORD")
     print(password)
-  
