9c9541800227f849e06bd3aa6078783226b864f0
ait1988 <ait1988@gmail.com>
2024-11-29 13:35:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def sayHello():
-    print("Hello World")
+# def sayHello():
+#     print("Hello World")
 
 
-def sayGoodbye():
-    print("Goodbye Ja")
+# def sayGoodbye():
+#     print("Goodbye Ja")
+
+import os
+
+
+def main():
+  my_name = os.getenv('MY_NAME')
+  my_secret = os.getenv('MY_SECRET')
+  print(f"Name: {my_name}")
+  print(f"Secret: {my_secret}")
+
+
+if __name__ == "__main__":
+  main()
